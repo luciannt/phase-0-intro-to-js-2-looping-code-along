2aca371a93542cddebecb8b3ad591a79818fb1d4
Lucianna Tittle <lucianna.roset@gmail.com>
2022-08-11 12:46:39 -0500

    commit tree 61acde2a776f736ac77a4a2cfed0f35209f5b2d5

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventNames) {
+  const message = [];
+
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return message;
+}
+
+function countDown(numbers) {
+  let i = 10;
+  while (i < numbers.length) {
+    console.log(numbers);
+  }
+  return numbers;
+}
